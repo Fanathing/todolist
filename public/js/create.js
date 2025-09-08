ef7f93c98a1a0e68dd839af2574bc3834e827908
Fanathing <mmybonnie@gamil.com>
2025-09-08 13:53:01 +0900

    Fix: create.js와 list.js에 생성일 기능 구현 및 수정 Fixed #2

--- public/js/create.js
+++ public/js/create.js
@@ -7,6 +7,7 @@ const setTodos = (e) => {
     const today = new Date();
     const listBirth = `${today.getFullYear()} ${today.getMonth()}월 ${today.getDate()}일`
     
+    
     const todo = {
         author: author.value,
         title: title.value,
